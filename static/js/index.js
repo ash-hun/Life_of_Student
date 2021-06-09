95b93274d68795c6180a29fb74cc8789fe7f77c0
JH9892 <jhchoi09@naver.com>
2021-06-10 00:56:49 +0900

    upload results
    upload results

--- static/js/index.js
+++
@@ -1,7 +0,0 @@
-// index.js
-const light = document.querySelector('.light');
-const container = document.querySelector('.container');
-
-light.onclick = function(){
-    container.classList.toggle('darkmode')
-}
